7e5f5a16c60a2c93fd1ce98f7d994d6a2b87c187
Luciano Silva Nascimento <CIANDT\lucianosn@lnb028741bhz.ciandt.global>
2021-08-19 21:17:51 -0300

    Initial version working.

---
+++ README.md
@@ -0,0 +1,43 @@
+# Motivation
+I created this app to help my GF to send ads to wpp groups, basically send limited ads in sequence with some interval.
+<br/>
+<br/>
+<br/>
+
+# How it work ?
+Each line in xlsx file is a ad, so just add name, image name, and text, it will send a image to wpp with a caption.
+It uses your account, so will be necessary load the qr code in your acc.
+<br/>
+<br/>
+<br/>
+
+# Configuration
+define your ads in ad.example.xlsx and rename ad.example.xlsx to ad.xlsx
+* define the group name and interval(in seconds) inside .env.example and rename .env.example to .env
+<br/>
+<br/>
+<br/>
+
+# How to make it run ?
+* Clone the project
+```
+    git clone https://github.com/luciano-nascimento/outlet-wpp-bot.git
+```
+* From the new folder created run npm install
+```
+    npm install
+```
+* rename your.env.example to .env
+* run it 
+```
+node app.js
+```
+* load the qr code from your wpp account
+* it will load de xlsx and send the ads
+<br/>
+<br/>
+<br/>
+
+# By the way
+Follow her sotre in instagram @noviacessorios  
+
---
+++ app.js
@@ -0,0 +1,67 @@
+require('dotenv').config()
+const venom = require('venom-bot');
+const xlsxFile = require('read-excel-file/node');
+const delayTime = process.env.INTERVAL
+const groupName = process.env.GROUP_NAME
+
+console.log(`[${getCurrentDateTime()}] Starting...`)
+console.log(`[${getCurrentDateTime()}] Group name defined : ${groupName}`)
+console.log(`[${getCurrentDateTime()}] Interval defined as : ${delayTime} seconds`)
+
+venom
+  .create()
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro)
+  });
+
+function start(client) {
+    client.getAllChatsGroups()
+        .then((result) => {
+            var group = result.filter(obj=> obj.name == groupName);
+            if (group.length > 0){
+                run(client, group);
+            }
+        })
+}
+
+const run = async (client, group) => {
+    const groupId = group[0].id.user + "@" + group[0].id.server
+    await xlsxFile('./ad.xlsx').then(async (rows) => {
+        for (const row of rows) {
+            const adName = row[0]
+            const imagePath = row[1]
+            const caption = row[2]
+            if(row[0] !== "AD"){
+                await sendImage(client, groupId, imagePath, imagePath, caption, adName)
+                await sleep(delayTime)        
+            }
+        }
+    })
+    process.exit()
+  }
+
+async function sendImage(client, to, imagePath, imageName, cc, adName){
+    try{
+        await client.sendImage(to, imagePath, imageName,cc)
+        console.log(`[${getCurrentDateTime()}] Ad ${adName} sent.`)
+        
+    } catch(error) {
+        console.error(`[${getCurrentDateTime()}] Error when sending ad: ${adName} :`, error) //return object error
+    }
+    
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function getCurrentDateTime(){
+    return new Date().toLocaleString()
+}
+
+
+
+
+
+// "Lançamento✨" +"\r\n\r\n"+ "🌵Brinco Folheado de"
